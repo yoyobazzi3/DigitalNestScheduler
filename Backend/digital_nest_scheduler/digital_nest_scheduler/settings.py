e6d8b0a38a880327201608ac416ae579fdc4085c
yoyobazzi3 <yoyobazzi3@gmail.com>
2024-11-26 16:38:52 -0800

    structure backend

--- Backend/digital_nest_scheduler/digital_nest_scheduler/settings.py
+++ Backend/digital_nest_scheduler/digital_nest_scheduler/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'projects',
+    'skills',
+    'schedules',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
