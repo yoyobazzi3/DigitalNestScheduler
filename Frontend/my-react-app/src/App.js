ce68b4b3ce78552686fa14a1c65027a97d0e9ba0
Ian Ellis <ianellis015@gmail.com>
2024-12-26 09:39:33 -0800

    (src):route-to-reccomendations

--- Frontend/my-react-app/src/App.js
+++ Frontend/my-react-app/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import React from 'react';
 import HomePage from './pages/HomePage';
+import NewProject from './components/NewProject/NewProject';
+import Recommendations from './pages/recommendations';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-      {/* Next Step: Import tools for routing. Start setting up the structure for the rest of the prototype */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/new-project" element={<NewProject />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ Frontend/my-react-app/src/components/Carousel/ImgCarousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Carousel = () => {
+    return (
+        <div className="image-carousel">
+            <p>[IMG Carousel]</p>
+          </div>
+    )
+}
+
+export default Carousel;
