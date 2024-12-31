548368e58160f600c934dd168f4b9c7fd2213879
Ian Ellis <ianellis015@gmail.com>
2024-12-31 13:15:34 -0800

    (src)pushing-status

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,5 +1,8 @@
 import baseCtrl from '../controllers/baseCtrl.js';
 import newProjectCtrl from '../controllers/newProjectCtrl.js';
+import addProjectCtrl from '../controllers/addProjectCtrl.js';
+// TODO seperate newProject and addProject Controllers in NewProjectCtrl.js
+// create a new file called addProject.js and set up a seperate route for that
 
 const routes = (app) => {
   app.route('/basePage')
@@ -8,8 +11,8 @@ const routes = (app) => {
   app.route('/newProject')
     .get(newProjectCtrl.newProject)
     
-  app.route('/newProject')
-    .post(newProjectCtrl.addProject)
+  app.route('/addProject')
+    .post(addProjectCtrl.addProject)
 
 
 };
