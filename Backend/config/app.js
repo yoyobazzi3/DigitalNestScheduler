62c17b4a7239b407c697520bd18ef0b57a243d43
yoyobazzi3 <yoyobazzi3@gmail.com>
2025-01-03 10:38:11 -0800

    feat(backend/app.js):added a rate limiter to not allow so many requests

--- Backend/config/app.js
+++ Backend/config/app.js
@@ -3,8 +3,19 @@ import express from 'express'
 import morgan from 'morgan'
 import routes from '../routes/routes.js'
 import cors from 'cors';
+import rateLimit from 'express-rate-limit'
 const app = express()
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again later',
+  });
+  
+
+
+
+app.use('/signup', limiter);
 app.use(morgan('dev'))
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
