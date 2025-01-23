8cf44b7c8f4a97b501f05087bf5b4810a2f3a9cd
Ian Ellis <ianellis015@gmail.com>
2025-01-22 18:08:00 -0800

    Place-holder layout until we find a better UI design

--- Frontend/my-react-app/src/components/Carousel/ImgCarousel.jsx
+++ Frontend/my-react-app/src/components/Carousel/ImgCarousel.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import './ImgCarousel.css'; // Assuming styles are stored here
 
-const Carousel = () => {
-    return (
-        <div className="image-carousel">
-            <p>[IMG Carousel]</p>
-          </div>
-    )
-}
+const App = () => {
+  return (
+    <div className="rectangle-container">
+      <div className="row">
+        <div className="rectangle rectangle-small">
+            <h2>Top Growth</h2>
+            <h3>By Location</h3>
+            </div>
+        <div className="rectangle rectangle-small">
+        <h2>Top Growth</h2>
+        <h3>By Department</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="rectangle rectangle-large">
+            <h1>Overall Team Growth</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Carousel;
+export default App;
