850783d00d4331991cbf74d035d3f7ba80859082
Lisandro Jimenez <jimenezlisandro88@gmail.com>
2025-02-13 13:18:37 -0800

    minor edit

--- Frontend/my-react-app/src/pages/recommendations.jsx
+++ Frontend/my-react-app/src/pages/recommendations.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import NavBar from '../components/Navbar/NavBar.jsx';
 import './Recommendations.css';
